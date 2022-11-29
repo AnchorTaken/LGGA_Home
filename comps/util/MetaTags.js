5db8111de314d190866d59a5c3b160dc64334440
Anchor <30292770+AnchorTaken@users.noreply.github.com>
2022-11-29 20:49:13 +0000

    small changes

--- comps/util/MetaTags.js
+++ comps/util/MetaTags.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import ogImage from "/public/assets/img/ogimage.jpg";
 
 export default function MetaTags({ title, desc, keywords }) {
   return (
@@ -8,15 +7,24 @@ export default function MetaTags({ title, desc, keywords }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="utf-8" />
         <title>{title}</title>
-        <link rel="icon" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/lzgga/image/upload/v1669754631/jw18mp4jtqxlqprfwjvh.png />"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="https://res.cloudinary.com/lzgga/image/upload/v1669754631/jw18mp4jtqxlqprfwjvh.png"
+        />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content={desc} />
         <meta name="keywords" content={keywords} />
         <meta name="author" content="ITRSQ" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={desc} />
-        <meta property="og:image" content="/public/assets/img/ogimage.jpg" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/lzgga/image/upload/v1669754877/ucyip1dcblqgeke09xrc.jpg"
+        />
       </Head>
     </>
   );
